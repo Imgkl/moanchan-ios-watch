7730ceda2950a9c8846e92ca7145ecd046e8f925
saigkl <gokul@1stmain.co>
2021-04-15 20:28:10 +0530

    Initial Commit

---
+++ moany WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  moany WatchKit Extension
+//
+//  Created by Gokul on 15/04/21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ moany WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  moany WatchKit Extension
+//
+//  Created by Gokul on 15/04/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
